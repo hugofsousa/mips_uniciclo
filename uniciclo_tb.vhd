ba602f81663ec341b82ab778e60a9f9ba86c10cf
Hugo Fagundes <hugofagundes07@gmail.com>
2017-02-05 14:55:53 -0200

    control without bne

--- uniciclo_tb.vhd
+++ uniciclo_tb.vhd
@@ -4,11 +4,11 @@ USE ieee.std_logic_1164.all;
 ENTITY uniciclo_tb IS
 END uniciclo_tb;
 ARCHITECTURE uniciclo_arch OF uniciclo_tb IS                                                
-SIGNAL clk : STD_LOGIC;
+SIGNAL clk, clk_mem : STD_LOGIC;
 SIGNAL display : STD_LOGIC_VECTOR(31 DOWNTO 0);
 COMPONENT uniciclo
 	PORT (
-		clk : IN STD_LOGIC;
+		clk, clk_mem : IN STD_LOGIC;
 		display : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
 	);
 END COMPONENT;
@@ -16,14 +16,26 @@ BEGIN
 	i1 : uniciclo
 	PORT MAP (
 		clk => clk,
+		clk_mem => clk_mem,
 		display => display
 	);
-init : PROCESS                                               
+
+PROCESS                                               
 BEGIN 
 	for i in 0 to 1000 loop
 		clk <= '0';
-		wait for 5 ns;
+		wait for 20 ns;
 		clk <= '1';
 	end loop;
-END PROCESS init;                                        
+END PROCESS;
+
+PROCESS                                               
+BEGIN 
+	for i in 0 to 1000 loop
+		clk_mem <= '0';
+		wait for 5 ns;
+		clk_mem <= '1';
+	end loop;
+END PROCESS;
+                                        
 END uniciclo_arch;
